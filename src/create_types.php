393ba1a8beb4e8a50518410b6864fd57d385bff9
PreethiPrakashA <40957676+PreethiPrakashA@users.noreply.github.com>
2020-03-28 22:44:43 +0530

    Create create_types.php

---
+++ src/create_types.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html >
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action ="create_types_post.php" method ="POST">
+      <label>Type Name </label>
+      <input type ="text" name ="typename" size ="25" placeholder ="Enter the typename"/>
+      <br>
+      <br>
+
+      <input type ="submit" value ="Submit" align ="center"/>
+    </form>
+
+  </body>
+</html>
