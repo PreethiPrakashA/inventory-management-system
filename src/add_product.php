5926a536e238a8d1a372b60989b56754cfda922b
PreethiPrakashA <40957676+PreethiPrakashA@users.noreply.github.com>
2020-03-28 22:54:05 +0530

    Create add_product.php

---
+++ src/add_product.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>                              //To do:check session
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form  action ="" method  ="">
+
+    <label>Product name: </label>
+    <input type = "text" name = "proname" size = "20"/>
+    <br>
+    <br>
+
+    <label>Product quantity: </label>
+    <input type ="text" name = "proquantity" size ="20"/>
+    <br>
+    <br>
+
+    <label>Product price:</label>
+    <input type = "text" name ="proprice" size = "20"/>
+    <br>
+    <br>
+
+  </form>
+
+  </body>
+</html>
