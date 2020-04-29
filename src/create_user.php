73f04afbd1cc7dc0ef4e6f8667027bfc20471a31
PreethiPrakashA <40957676+PreethiPrakashA@users.noreply.github.com>
2020-04-29 09:04:20 +0530

    Update create_user.php

--- src/create_user.php
+++ src/create_user.php
@@ -1,32 +1,45 @@
 <!DOCTYPE html>
+<?php
+	session_start();
+	if(!isset($_SESSION['logged_in'])) {
+		header("Location: index.php");
+		//if a user is not logged in he will be redirected to index.php
+	}
+	elseif($_SESSION['logged_in']!== 0) {
+		die("ERROR : You do not have sufficient
+		 privelege to access this");
+	}
+
+?>
+
 <html lang="en" dir="ltr">
 	<head>
 		<meta charset="utf-8">
 		<title></title>
+		<link rel ="stylesheet" href = "css_inven/styles.css">
 	</head>
 		<body>
-		 <link rel ="stylesheet" href = "css_inven/styles.css">
 
 		<form action ="create_user_post.php" method = "POST">
 			<label>Username</label>
 			<input type ="text" name ="uname" size = "25" placeholder="enter your username  here" />
-			<br>
-			<br>
+			<br />
+			<br />
 
 			<label>Password</label>
 			<input type ="password" name ="pass" size="25" placeholder ="enter your password here" />
-			<br>
-			<br>
+			<br />
+			<br />
 
 			<label>First name</label>
 			<input type ="text" name ="fname" size="25" placeholder ="enter your first name" />
-			<br>
-			<br>
+			<br />
+			<br />
 
 			<label>Last name </label>
 			<input type ="text" name ="lname" size ="25" placeholder ="enter your last name" />
-			<br>
-			<br>
+			<br />
+			<br />
 
 			<label>Role</label>
 			<select name ="role" >
@@ -34,10 +47,10 @@
 				<option>Producer</option>
 				<option>Salesman</option>
 			</select>
-			<br>
-			<br>
+			<br />
+			<br />
 			<input type ="submit" value ="submit" />
-			<br>
+			<br />
 
 		</form>
 
