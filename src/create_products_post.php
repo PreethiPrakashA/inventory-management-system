4a5e2d2d163d266b5a05cf6604c38902be92d3c7
PreethiPrakashA <40957676+PreethiPrakashA@users.noreply.github.com>
2020-04-29 08:59:50 +0530

    Update create_products_post.php

--- src/create_products_post.php
+++ src/create_products_post.php
@@ -1,5 +1,14 @@
 <?php
-    $mysqlconn = new mysqli('localhost','root','','inventory');
+    session_start();
+    if(!isset($_SESSION['logged_in'])) {
+        header("Location: index.php");
+    }
+    elseif(($_SESSION['logged_in'] !== 0) && ($_SESSION['logged_in'] !== 1)) {
+        die("ERROR : You do not have sufficient
+         privelege to access this");
+    }
+
+    $mysqlconn = new mysqli('localhost', 'root', '', 'inventory');
     if ($mysqlconn->connect_error) {
         die("Connection failed: " . $mysqlconn->connect_error);
     }
@@ -8,7 +17,7 @@
     $proname = $_POST['pro_name'];
     $proprice = $_POST['pro_price'];
     $quantity = $_POST['quantity'];
-    $mysqlconn->query("INSERT INTO products VALUES(DEFAULT, '$proname', '$proprice', 1 , '$quantity')");
+    $mysqlconn->query("INSERT INTO products VALUES(DEFAULT, '$proname', '$proprice', 1, '$quantity')");
     $mysqlconn->close();
 
 ?>
