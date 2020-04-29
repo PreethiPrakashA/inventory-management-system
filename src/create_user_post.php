7df0e61860d61237db5a883a5de74b548119e750
PreethiPrakashA <40957676+PreethiPrakashA@users.noreply.github.com>
2020-04-29 09:06:23 +0530

    Update create_user_post.php

--- src/create_user_post.php
+++ src/create_user_post.php
@@ -1,7 +1,16 @@
 <?php
+	session_start();
+	if(!isset($_SESSION['logged_in'])) {
+        header("Location: index.php");
+    }
+	elseif($_SESSION['logged_in'] !== 0) {
+		die("ERROR : You do not have sufficient
+		 privelege to access this");
+	}
+
 	$conn = new mysqli('localhost', 'root', '', 'inventory');
 	if($conn->connect_error) {
-		die("connection failed" .$connect_error);
+		die("connection failed" . $connect_error);
 	}
 	echo "User created successfully";
 
@@ -23,7 +32,14 @@
 		$value	=	2;
 	}
 
-	$conn->query("INSERT INTO users VALUES(DEFAULT, '$uname', '$pass', '$fname', '$lname', '$value')");
+	$result = $conn->query("SELECT * FROM users WHERE username = '$uname'");
+	if($result->num_rows > 0) {
+		echo "The username already exists";
+	}
+	else {
+		$conn->query("INSERT INTO users VALUES(DEFAULT, '$uname', '$pass', '$fname', '$lname', '$value')");
+	}
+	$result->close();
 	$conn->close();
 
  ?>
