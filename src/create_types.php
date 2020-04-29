fe87f8e588e6165b4021cf799fa90d57e997b528
PreethiPrakashA <40957676+PreethiPrakashA@users.noreply.github.com>
2020-04-29 09:01:15 +0530

    Update create_types.php

--- src/create_types.php
+++ src/create_types.php
@@ -1,18 +1,36 @@
 <!DOCTYPE html >
+<?php
+    session_start();
+    if(!isset($_SESSION['logged_in'])) {
+        header("Location: index.php");
+    }
+    elseif(($_SESSION['logged_in'] !== 0) && ($_SESSION['logged_in'] !== 1)) {
+        die("ERROR : You do not have sufficient
+         privelege to access this");
+    }
+ ?>
+
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <form action ="create_types_post.php" method ="POST">
-      <label>Type Name </label>
-      <input type ="text" name ="typename" size ="25" placeholder ="Enter the typename"/>
-      <br>
-      <br>
+    <head>
+        <style>
+         body {
+           background-color: #aeefec;
+         }
+         a:active {
+           color: #ff926b;
+         }
+       </style>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <form action ="create_types_post.php" method ="POST">
+            <label>Type Name </label>
+            <input type ="text" name ="typename" size ="25" placeholder ="Enter the typename"/>
+            <br />
+            <br />
 
-      <input type ="submit" value ="Submit" align ="center"/>
-    </form>
-
-  </body>
+            <input type ="submit" value ="Submit" align ="center"/>
+        </form>
+    </body>
 </html>
