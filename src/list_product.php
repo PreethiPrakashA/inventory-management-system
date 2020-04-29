e2ccf1064e778fba3b4847f825c83a49224aed27
PreethiPrakashA <40957676+PreethiPrakashA@users.noreply.github.com>
2020-04-29 09:18:13 +0530

    Update list_product.php

--- src/list_product.php
+++ src/list_product.php
@@ -1,4 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <style>
+         body {
+           background-color: #aeefec;
+         }
+         a:active {
+           color: #ff926b;
+         }
+       </style>
+
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+
+    </body>
+</html>
 <?php
+
     $mysqlconn = new mysqli('localhost','root','','inventory');
     if ($mysqlconn->connect_error) {
         die("Connection failed: " . $mysqlconn->connect_error);
@@ -10,16 +30,27 @@
 
       //  $row = 'A';
         //  WHILE($row!=FALSE)
+        echo '<table>';
+        echo '
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Type</th>
+                    <th>Quantity</th>
+                </tr>';
         while(($row = $result->fetch_assoc()) != FALSE) {
             //  $row = $result->fetch_assoc();
-                  echo $row['pro_id'] .'&emsp;';
-                  echo '<b>';  echo $row['pro_name']."&emsp;"; echo'</b>';
-                  echo $row['pro_price']."&emsp;";
-                  echo $row['typeid']."&emsp;";
-                  echo $row['quanttiy']."&emsp;";
-                 echo '<br>';
-                }
-     }
+            echo '<tr>';
+            echo '<td>' . $row['pro_id'] . '</td>';
+            echo '<td>' . $row['pro_name'] . '</td>';
+            echo '<td>' . $row['pro_price'] . '</td>';
+            echo '<td>' . $row['typeid'] . '</td>';
+            echo '<td>' . $row['quanttiy'] . '</td>';
+            echo '</tr>';
+        }
+        echo '</table>';
+     } else echo 'There are currently no products!';
 
     $result->close();
     $mysqlconn->close();
